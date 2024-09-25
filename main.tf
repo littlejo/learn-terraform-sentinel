2f1256dbd90ab3182cbc0f9d7ec6976693111b08
Joseph Ligier <joseph.ligier@accenture.com>
2024-09-25 10:31:08 +0200

    test

--- main.tf
+++ main.tf
@@ -10,3 +10,15 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "aaa"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = [for k, v in ["us-east-1a", "us-east-1b"] : cidrsubnet("10.0.0.0/16", 4, k)]
+
+  tags = local.tags
+}
